b84fc285695ff6614986762964420d5bcc00d38d
rekhilahana34-tech <rekhilahana34@gmail.com>
2026-03-12 13:05:09 -0400

    les méthodes de la classe « Employe »

--- 2.py
+++ 2.py
@@ -4,3 +4,28 @@ class Employe:
         self.nom = nom
         self.prenom = prenom
         self.voitureService = None
+
+    def afficherInformation(self):
+        print("Numero permis :", self.numeroPermis)
+        print("Nom :", self.nom)
+        print("Prenom :", self.prenom)
+
+        if self.voitureService is not None:
+            print("Voiture :", self.voitureService.matricule)
+        else:
+            print("pas de voiture de service")
+
+    def affecterVoiture(self, voiture):
+        if self.voitureService is None and voiture.chauffeur is None:
+            self.voitureService = voiture
+            voiture.chauffeur = self
+            print("voiture affectée")
+        else:
+            print("Impossible de faire voiture")
+
+    def retirerVoiture(self):
+        if self.voitureService is not None:
+            self.voitureService.chauffeur = None
+            self.voitureService = None
+
+        print("voiture retirer")
